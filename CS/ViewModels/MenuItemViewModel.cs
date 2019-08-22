e3f7fd1bbd2576e3361f6b5d7c01cd0056616d9c
goncharov.vasily <vasily.goncharov@devexpress.com>
2019-08-22 14:17:49 +0300

    add example content

---
+++ CS/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using DevExpress.Mvvm;
+
+namespace DXSample.ViewModels {
+    public class MenuItemViewModel : ViewModelBase {
+        public MenuItemViewModel() {
+            Items = new ObservableCollection<MenuItemViewModel>();
+        }
+
+        public string Title {
+            get { return GetProperty(() => Title); }
+            set { SetProperty(() => Title, value); }
+        }
+
+        public ObservableCollection<MenuItemViewModel> Items {
+            get { return GetProperty(() => Items); }
+            set { SetProperty(() => Items, value); }
+        }
+    }
+}
---
+++ CS/Views/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace DXSample.Views {
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : UserControl {
+        public MainView() {
+            InitializeComponent();            
+        }
+    }
+}
